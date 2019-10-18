b0479aecd57eb6ad4bff34646a9dc11696b898a1
Kundan Rupakheti <kundan.rupakheti99@gmail.com>
2019-10-18 18:58:45 -0500

    checking out the implementation soon

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html>
 <head>
   <title>Home Bazaar</title>
+  
 </head>
 <body>
+<p> we are ready for out project </p>
   <h1 style="text-align: align:'enter';">Hamro Bazzar</h1>
 </body>
 </html>
