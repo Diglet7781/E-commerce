5786e2ded578bfe6d873e8b34f9bf5c270a46e6d
francafu <54951768+francafu@users.noreply.github.com>
2019-11-09 23:46:32 -0600

    uses js and php to include navbar and footer

---
+++ Frontend/php/footer.php
@@ -0,0 +1,53 @@
+<footer class="footer">
+   <div class="container">
+      <div class="row gap-y align-items-center">
+         <div class="col-6 col-lg-3">
+            <a href="../index.html"><img src="../img/nav3.png" alt="logo"></a>
+         </div>
+         <div class="col-6 col-lg-3 text-right order-lg-last">
+            <div class="social text-center text-lg-right">
+               <a class="social-facebook" href="https://www.facebook.com"><i class="fa fa-facebook"></i></a>
+               <a class="social-twitter" href="https://twitter.com"><i class="fa fa-twitter"></i></a>
+               <a class="social-instagram" href="https://www.instagram.com"><i class="fa fa-instagram"></i></a>
+            </div>
+         </div>
+         <div class="col-lg-6">
+            <div class="nav nav-bold nav-uppercase nav-trim justify-content-lg-center">
+               <a class="nav-link" href="">Customer Services</a>
+               <a class="nav-link" href="">About Us</a>
+               <a class="nav-link" href="">Help</a>
+               <a class="nav-link" href="">Follow</a>
+               <a class="nav-link" href="">Subscribe</a>
+            </div>
+         </div>
+      </div>
+   </div>
+</footer>
+<!-- /.footer -->
+<!-- Scripts -->
+<script src="../js/page.min.js"></script>
+<script src="../js/script.js"></script>
+</footer>
+<footer class="site-footer">
+   <div class="container">
+      <div class="row gap-y">
+         <div class="col-12 col-md-6">
+            <p class="text-center text-md-left">Copyright © 2017 <a class="text-dark" href="http://bazaar.com">BaZaar</a>. All rights reserved.</p>
+         </div>
+         <div class="col-12 col-md-6">
+            <ul class="nav nav-inline nav-primary nav-dotted nav-dot-separated justify-content-center justify-content-md-end">
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Terms of use</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Privacy policy</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#">About</a>
+               </li>
+            </ul>
+         </div>
+      </div>
+   </div>
+</footer>
+
