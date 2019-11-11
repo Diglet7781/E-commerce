2335ff76e07202f5c44f4368189a1c124ba296e8
raajankhadka <raajankhadka@gmail.com>
2019-11-11 12:56:41 -0600

    logout.php

--- logout.php
+++ logout.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -19,26 +18,4 @@
             <a href="login.php">Log in</a> again.
         </p>
     </body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Logged Out</title>
-    </head>
-    <?php
-        session_start();
-        session_destroy();
-        
-        
-    ?> 
-    <body>
-        <h1>Logged Out</h1>
-        <p>
-            You are now logged out of the website.
-        </p>
-        <p>
-            <a href="login.php">Log in</a> again.
-        </p>
-    </body>
->>>>>>> da917de332e392130c5859671427e596e6f23ce7
 </html>
