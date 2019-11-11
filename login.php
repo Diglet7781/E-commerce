c18f05f9ee571539e288e12d1c6d97e9abd0e127
raajankhadka <raajankhadka@gmail.com>
2019-11-11 12:56:10 -0600

    login.php

--- login.php
+++ login.php
@@ -23,20 +23,13 @@
     session_start();
     require_once "functions/validate.php";
     require_once "dblogin.php";
-<<<<<<< HEAD
    
-=======
-
-   
-
->>>>>>> da917de332e392130c5859671427e596e6f23ce7
     if(isset($_POST['submit'])){
         $username=test_input($_POST['username']);
         $password=test_input($_POST['password']);
         
         //creating conncetion to the db
         $connect= createConn();
-<<<<<<< HEAD
         $query="SELECT * From user where username='$username';";
         $result= $connect->query($query);
         if($result->num_rows==1){
@@ -44,28 +37,10 @@
            if (password_verify($password,$row['password'])){
               $_SESSION['username']=$row['username'];
               $type= $_SESSION['type']=$row['accountType'];
-=======
-
-        $query="SELECT * From user where username='$username';";
-
-        $result= $connect->query($query);
-
-        if($result->num_rows==1){
-            $row=$result->fetch_assoc();
-
-           if (password_verify($password,$row['password'])){
-              $_SESSION['username']=$row['username'];
-              $type= $_SESSION['type']=$row['accountType'];
-
->>>>>>> da917de332e392130c5859671427e596e6f23ce7
               switch ($type){
                   case 'seller':
                   header('Location:signup.php');
                   exit();
-<<<<<<< HEAD
-=======
-
->>>>>>> da917de332e392130c5859671427e596e6f23ce7
                   case 'buyer':
                   header('Location:buyer_home.php');
                   exit();
@@ -73,20 +48,10 @@
            }else{
                echo "password did not match";
            }
-<<<<<<< HEAD
-=======
-
-
->>>>>>> da917de332e392130c5859671427e596e6f23ce7
         }
         else{
             echo "please enter the correct credentials or create a new account";
         }
-<<<<<<< HEAD
-=======
-
-
->>>>>>> da917de332e392130c5859671427e596e6f23ce7
     }else{
         echo "hello";
     }
