22f9ec4262ce2cecae6a14b97443f7de14e4dda9
raajankhadka <raajankhadka@gmail.com>
2019-11-11 12:39:40 -0600

    login.php

--- login.php
+++ login.php
@@ -9,13 +9,49 @@
 <body>
     <form method="post" action="login.php">
     username: 
-    <input type="text" placeholder="username">
+    <input type="text" name="username" placeholder="username">
     <br>
     password:
-    <input type="password" placeholder="password">
+    <input type="password" name="password" placeholder="password">
     <br>
-    <button name="login" type="submit">Login</button>
+    <input type="submit" name="submit" name="submit" value="Login">
 </form>
 
 </body>
 </html>
+<?php
+    session_start();
+    require_once "functions/validate.php";
+    require_once "dblogin.php";
+   
+    if(isset($_POST['submit'])){
+        $username=test_input($_POST['username']);
+        $password=test_input($_POST['password']);
+        
+        //creating conncetion to the db
+        $connect= createConn();
+        $query="SELECT * From user where username='$username';";
+        $result= $connect->query($query);
+        if($result->num_rows==1){
+            $row=$result->fetch_assoc();
+           if (password_verify($password,$row['password'])){
+              $_SESSION['username']=$row['username'];
+              $type= $_SESSION['type']=$row['accountType'];
+              switch ($type){
+                  case 'seller':
+                  header('Location:signup.php');
+                  exit();
+                  case 'buyer':
+                  header('Location:buyer_home.php');
+                  exit();
+              }
+           }else{
+               echo "password did not match";
+           }
+        }
+        else{
+            echo "please enter the correct credentials or create a new account";
+        }
+    }else{
+        echo "hello";
+    }
