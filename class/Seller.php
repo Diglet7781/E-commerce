829e58a22fca1e801cf7388578705a02de6658a1
raajankhadka <raajankhadka@gmail.com>
2019-11-12 17:56:37 -0600

    made some changes in seller.php

--- class/Seller.php
+++ class/Seller.php
@@ -1,6 +1,6 @@
 <?php
 include "User.php";
-
+require_once "dblogin.php";
 class Seller extends User{
     //properties
     private $sellerID;
@@ -9,14 +9,44 @@ class Seller extends User{
     
 
     //methods
+    public function __construct($productName,$productId,$productDescription,$productQuantity,$productPrice,$productImage) {
+        $this->productName = $productName;
+        $this->productID = $productId;
+        $this->productDescription = $productDescription;
+        $this->productQuantity = $productQuantity;
+        $this->productPrice=$productPrice;
+        $this->productImage = $productImage;
+        
+      }
 
-
-    public function __construct($sellerID,$accountBalance,$totalAddedProduct){
-        $this->sellerID = $sellerID;
-        $this->accountBalance=$accountBalance;
-        $this->totalAddedProduct=$accountBalance;
+   
+    //get product name
+    public function getproductName(){
+        return $this->productName;
+    }
+    //get product ID
+    public function getproductId(){
+        return $this->productId;
     }
 
+    //get product description
+    public function getproductDescription(){
+        return $this->productDescription;
+    }
+
+    //get product quantity
+    public function getproductQuantity(){
+        return $this->productQuantity;
+    }
+
+    //get product price
+    public function getproductPrice(){
+        return $this->productprice;
+    }
+    //get product image
+    public function getproductImage(){
+        return $this->productImage;
+    }
 
     //get seller ID
     public function getsellerID(){
@@ -31,6 +61,21 @@ class Seller extends User{
     //get totaladdedProduct
     public function totalAddedProduct(){
         return $this->totalAddedProduct;
+    }
+    //add items to inventory
+    public function addItems(){
+        $productName= $this->getproductName();
+        $productId=$this->getproductId();
+        $productDescription=$this->getproductDescription();
+        $productQuantity=$this->getproductQuantity();
+        $productPrice=$this->getproductPrice();
+        $productImage=$this->getproductImage();
+        
+        $query="INSERT INTO inventory(productid, productname, productdescription,quantity,price,picture) 
+        VALUES ('$productId','$productName','$productDescription','$productQuantity','$productPrice','$productImage')";
+       
+        return $query;
+
     }
     
 
