f0535689f9359664fc672d6e527ee47c868e5ec6
francafu <54951768+francafu@users.noreply.github.com>
2019-11-14 23:34:10 -0600

    file dump, nothing important

---
+++ attempt at version control/navbar.php
@@ -0,0 +1,70 @@
+<nav class="navbar navbar-expand-lg navbar-dark navbar-stick-dark" data-navbar="smart">
+   <div class="container">
+      <div class="navbar-left">
+         <button class="navbar-toggler" type="button">&#9776;</button>
+         <a class="navbar-brand" href="">
+            <img class="logo-dark" src="../img/nav2.png" alt="logo"> 
+            <h2> BaZaar</h2>
+         </a>
+      </div>
+      <section class="navbar-mobile">
+      <span class="navbar-divider d-mobile-none"></span>
+      <ul class="nav nav-navbar">
+         <li class="nav-item">
+            <a class="nav-link" href="#"> Home </a>
+            <ul class="nav">
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Option 1 </span></a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Option 2 </span></a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Option 3</span></a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#"> Option 4 </span></a>
+               </li>
+            </ul>
+         </li>
+         <li class="nav-item">
+            <a class="nav-link" href="#"> Home </a>
+            <ul class="nav">
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Option 1 </span></a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Option 2 </span></a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Option 3</span></a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#"> Option 4 </span></a>
+               </li>
+            </ul>
+         </li>
+         <li class="nav-item">
+            <a class="nav-link" href="#"> Home </a>
+            <ul class="nav">
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Option 1 </span></a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Option 2 </span></a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#">Option 3</span></a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#"> Option 4 </span></a>
+               </li>
+            </ul>
+         </li>
+      </ul>
+      <!--check-->
+      <a class="btn btn-xs btn-round btn-success" href="">Login</a>
+      <a class="btn btn-xs btn-round btn-success" href="">Signup</a>
+   </div>
+</nav>
+
