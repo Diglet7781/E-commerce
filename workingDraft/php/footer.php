6b87fe0db73f2b659dba1757dccb2d139c6236df
francafu <54951768+francafu@users.noreply.github.com>
2019-11-14 21:41:35 -0600

    user profile, login, product page

---
+++ workingDraft/php/footer.php
@@ -0,0 +1,57 @@
+<!-- footer -->
+<div id="footerReuse" class="container-fluid navbar-fixed-bottom navbar-inverse">
+  <div class="row">
+        <div class="col-md-1">
+      <!-- First of three columns -->
+      <h5 class="text-warning">BaZaar ©2019</h5>
+<a class="navbar-brand" href="#">
+               <img src="../img/nav2.png" width="90" height="90" alt="Bazaar Logo" style="padding-top: 15%">
+               </a>
+    </ul>
+    </div>
+    <div class="col-md-1">
+      <!-- First of three columns -->
+      <h5 class="text-warning">Contact Us</h5>
+                <ul>
+      <li><a class="text-white" href="#">Email 1</a></li>
+      <li><a class="text-white" href="#">Email 2</a></li>
+      <li><a class="text-white" href="#">Email 3</a></li>
+      <li><a class="text-white" href="#">Email 4</a></li>
+    </ul>
+    </div>
+    <div class="col-md-1">
+    </div>
+    <div class="col-md-3">
+      <!-- First of three columns -->
+      <h5 class="text-warning">Features</h5>
+                <ul>
+      <li><a class="text-white" href="#">Cool stuff</a></li>
+      <li><a class="text-white" href="#">Random feature</a></li>
+      <li><a class="text-white" href="#">Team feature</a></li>
+      <li><a class="text-white" href="#">Stuff for developers</a></li>
+      <li><a class="text-white" href="#">Another one</a></li>
+      <li><a class="text-white" href="#">Last time</a></li>
+    </ul>
+    </div>
+    <div class="col-md-3">
+      <!-- Second of three columns -->
+          <h5 class="text-warning">Resources</h5>
+          <ul >
+      <li><a class="text-white" href="#">Resource</a></li>
+      <li><a class="text-white" href="#">Resource name</a></li>
+      <li><a class="text-white" href="#">Another resource</a></li>
+      <li><a class="text-white" href="#">Final resource</a></li>
+    </ul>
+    </div>
+    <div class="col-md-3">
+      <!-- Third of three columns -->
+          <h5 class="text-warning">About</h5>
+    <ul >
+      <li><a class="text-white" href="#">Team</a></li>
+      <li><a class="text-white" href="#">Locations</a></li>
+      <li><a class="text-white" href="#">Privacy</a></li>
+      <li><a class="text-white" href="#">Terms</a></li>
+    </ul>
+    </div>
+  </div>
+</div>
---
+++ workingDraft/php/navbar.php
@@ -0,0 +1,41 @@
+<!-- navbar -->
+<nav class="navbar-fixed-top navbar-default navbar-inverse">
+   <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+   <span class="sr-only">Toggle navigation</span>
+   <span class="icon-bar"></span>
+   <span class="icon-bar"></span>
+   <span class="icon-bar"></span>
+   </button>
+   <a class="navbar-brand" href="#">
+   <img src="../img/nav2.png" width="90" height="90" alt="Bazaar Logo" style="padding-top: 15%">
+   </a>
+
+   <!-- Collect the nav links, forms, and other content for toggling -->
+   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+         <li><a href="#">Link</a></li>
+         <li><a href="#">Link</a></li>
+         <li><a href="#">Link</a></li>
+         <li><a href="#">Link</a></li>
+         <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+               <li><a href="#">Action</a></li>
+               <li><a href="#">Another action</a></li>
+               <li><a href="#">Something else here</a></li>
+               <li role="separator" class="divider"></li>
+               <li><a href="#">Separated link</a></li>
+               <li role="separator" class="divider"></li>
+               <li><a href="#">One more separated link</a></li>
+            </ul>
+         </li>
+      </ul>
+      <form class="navbar-form navbar-right">
+         <div class="form-group">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+         </div>
+         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+   </div>
+   <!-- /.navbar-collapse -->
+</nav>
