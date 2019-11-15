d7f0d2129226535d1fc4131c275ea6bc71fa1768
francafu <54951768+francafu@users.noreply.github.com>
2019-11-14 23:38:20 -0600

    file dump, nothing important

---
+++ attempt at version control/php/footer.php
@@ -0,0 +1,23 @@
+<div class="footer">
+  <div id="button"></div>
+<div id="container">
+
+<div class="footer_center">
+
+     
+        <div class="container">
+            <div class="row gap-y align-items-center">
+
+            <div class="col-8 col-lg-3">
+                <a href="../index.html"><img src="../img/nav3.png"  width="80" height="80" alt="logo"></a>
+                
+            
+            </div>
+            </div>
+        </div>
+        
+        
+
+</div>
+</div>
+</div>
