e8a946ee5d87323e5a27f3c89de8ab286127fd4b
raajankhadka <raajankhadka@gmail.com>
2019-11-16 20:05:45 -0600

    some more chane

--- viewInventory.php
+++ viewInventory.php
@@ -26,7 +26,9 @@
     $connect = createConn();
 
    $sqlBooks= "SELECT * FROM inventory WHERE productType='book'";
+   $sqlApparel="SELECT * FROM inventory WHERE productType='apparel'";
    $reasult = $connect->query($sqlBooks);
+   $reasult2=$connect->query($sqlApparel);
 
     echo"<table>";
     echo "<tr>";
@@ -56,6 +58,37 @@
             echo '<td><a href="editInventory.php?' .SID. '">Update</a></td>';
             echo '<td><a href="deleteItem.php?'.SID.'">Delete</a></td>';
         echo "</tr>";
+        echo"</table>";
+        echo"<table>";
+        echo "<tr>";
+        echo    "<th>Product ID</th>";
+        echo    "<th>Product Type</th>";
+        echo    "<th>Product Name</th>";
+        echo    "<th>Product Description</th>";
+        echo    "<th>quantity</th>";
+        echo    "<th>price per item</th>";
+        echo    "<th>pictures</th>";
+        echo    "<th>seller ID</th>";
+         echo"</tr>";
+         while($row = $reasult2->fetch_assoc()){
+            $_SESSION['id']=$row["productid"];
+            
+            echo   "<tr>";
+             echo    "<td>" . $row["productid"]. "</td>";
+             echo    "<td>" . $row["productType"].  "</td>";
+             echo    "<td>" . $row["productName"]. "</td>";
+             echo    "<td>" . $row["productDescription"]. "</td>";
+             echo    "<td>" . $row["quantity"]. "</td>"; 
+             echo    "<td>" . $row["price"]. "</td>";
+             echo    "<td>";
+             echo "<img src='" . $row['picture'] . "'>";
+             echo "</td>";
+             echo    "<td>" . $row["sellerid"]. "</td>";
+             echo '<td><a href="editInventory.php?' .SID. '">Update</a></td>';
+             echo '<td><a href="deleteItem.php?'.SID.'">Delete</a></td>';
+         echo "</tr>";
+        echo"</table>";
+
    	}
 
    $connect->close();
