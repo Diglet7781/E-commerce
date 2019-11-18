6b46798750bb90137054dff69c6c10fbb9651645
Diglet7781 <kiran.thapa7781@gmail.com>
2019-11-17 18:18:20 -0600

    updated comments

--- viewdetails.php
+++ viewdetails.php
@@ -18,14 +18,20 @@ $row=$result->fetch_assoc();
     $description=$row['productDescription'];
     $image=$row['picture'];
 
+    //product image
 echo "<img style='height:50px;'src='".$image."'>";  
 echo "<br>";  
+//product name
 echo $productName;
 echo "<br>";
+//product price
 echo $price;
 echo "<br>";
+//prodouct availibility
 echo "available quantity :". $availableQuantity;
 echo "<br>";
+//product description
 echo $description;
 echo "<br>";
+//add to cart option
 echo "<a href='addtocart.php?productId=".$productId."'>"."Add to cart". "</a>";
